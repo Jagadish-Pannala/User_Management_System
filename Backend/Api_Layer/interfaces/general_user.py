13920a248973789e47178fb07f7841a0aed32293
smdteja <smdedu2003@gmail.com>
2025-10-07 17:02:07 +0530

    Profile Updated 2

--- Backend/Api_Layer/interfaces/general_user.py
+++ Backend/Api_Layer/interfaces/general_user.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class EditProfile(BaseModel):
     first_name: str
     last_name: str
     contact: str
-    password: str
+    password: Optional[str] = None
 
 class EditProfileHr(BaseModel):
     first_name: str
